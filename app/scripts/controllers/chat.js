52ce199bc2e1d3546b805060071cfdca26ec0b63
Benjamin <BenjDaniel90@gmail.com>
2015-12-29 22:37:31 +0100

    [CHAT] Add support shift+enter != enter Change display

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -7,41 +7,31 @@
  * A demo of using AngularFire to manage a synchronized list.
  */
 angular.module('bitchizzApp')
-  .controller('ChatCtrl', function ($scope, user, Ref, $firebaseArray, $firebaseObject, $timeout) {
+  .controller('ChatCtrl', function ($scope, $log, user, Ref, $firebaseArray, $firebaseObject, $timeout) {
     var profile = $firebaseObject(Ref.child('users/' + user.uid));
 
     // synchronize a read-only, synchronized array of messages, limit to most recent 10
     $scope.messages = $firebaseArray(Ref.child('messages').limitToLast(100));
 
     // display any errors
-    $scope.messages.$loaded().then(function() {
+    $scope.messages.$loaded().then(function () {
       //scroll to bottom
       var elmt = $('.messageArea');
-      $timeout(function() {
+      $timeout(function () {
         elmt.scrollTop(elmt.scrollHeight);
-      },100);
+      }, 100);
     }).catch(alert);
 
-    // provide a method for adding a message
-    $scope.addMessage = function (newMessage) {
-      if (newMessage) {
-        // push a message to the end of the array
-        $scope.messages.$add({
-          author: profile.name,
-          timestamp: parseInt(new Date().getTime() / 1000),
-          text: newMessage
-        }).catch(alert);
-      }
-    };
 
     $scope.$watch($scope.getWindowDimensions, function (newValue, oldValue) {
       var dims = $scope.getWindowDimensions();
       $scope.heightChat = 0;
       $scope.heightChat -= $('.header').outerHeight(true);
       $scope.heightChat -= $('.footer').outerHeight(true);
-      $scope.heightChat -= $('div[ng-view]>h2').outerHeight(true);
-      $scope.heightChat -= $('div[ng-view]>form').outerHeight(true);
-      $scope.heightChat -= $('div[ng-view]>p').outerHeight(true);
+      $scope.heightChat -= $('div[ng-view]>div>h2').outerHeight(true);
+      $scope.heightChat -= $('div[ng-view]>div>.textareaParent').outerHeight(true);
+      $scope.heightChat -= $('div[ng-view]>div>p').outerHeight(true);
+      $scope.heightChat -= 10; //10px margin-bottom
       $scope.heightChat += dims.h;
     }, true);
 
@@ -51,4 +41,26 @@ angular.module('bitchizzApp')
         $scope.err = null;
       }, 5000);
     }
+
+    $scope.onKeyUp = function (event) {
+      var textarea = event.target;
+      var content = textarea.value;
+      if (event.keyCode == 13 && event.shiftKey) {
+        event.stopPropagation();
+      } else if (event.keyCode == 13) {
+        addMessage(content.substring(0, content.length-1));
+        textarea.value = null;
+      }
+    }
+
+    function addMessage(newMessage) {
+      if (newMessage) {
+        // push a message to the end of the array
+        $scope.messages.$add({
+          author: profile.name,
+          timestamp: parseInt(new Date().getTime() / 1000),
+          text: newMessage
+        }).catch(alert);
+      }
+    };
   });
