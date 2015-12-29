899c04dc4c990c432a3bd98a82115c11b27dab33
Benjamin <BenjDaniel90@gmail.com>
2015-12-29 22:40:29 +0100

    [CHAT] Add support shift+enter != enter Change display

---
+++ app/scripts/directives/resize.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name bitchizzApp.directive:resize
+ * @description
+ * # resize
+ */
+angular.module('bitchizzApp')
+  .directive('resize', function ($window) {
+    return function (scope, element) {
+      var w = angular.element($window);
+      scope.getWindowDimensions = function () {
+        return {
+          'h': w.height(),
+          'w': w.width()
+        };
+      };
+
+      w.bind('resize', function () {
+        scope.$apply();
+      });
+    }
+  })
