f3edc1d75b49849a7ebcb00c488259d01c996b10
Benjamin <BenjDaniel90@gmail.com>
2016-01-09 16:41:11 +0100

    [PROFILE] Update profile, add photo and Facebook name

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,13 +7,25 @@
  * Manages authentication to any active providers.
  */
 angular.module('bitchizzApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location) {
-    $scope.oauthLogin = function(provider) {
+  .controller('LoginCtrl', function ($scope, Auth, $location, Ref, $firebaseObject) {
+    $scope.oauthLogin = function (provider) {
       $scope.err = null;
       Auth.$authWithOAuthPopup(provider, {rememberMe: true}).then(redirect, showError);
     };
 
-    function redirect() {
+    function redirect(auth) {
+      var uid = auth.uid;
+      var facebookObj = auth.facebook;
+      var name = facebookObj.cachedUserProfile.first_name + ' ' + facebookObj.cachedUserProfile.last_name[0];
+
+      var profile = $firebaseObject(Ref.child('users/' + uid));
+
+      profile.name = name;
+      profile.profileImageURL = facebookObj.profileImageURL;
+
+      profile.$save();
+      //profile.set();
+
       $location.path('/account');
     }
 
