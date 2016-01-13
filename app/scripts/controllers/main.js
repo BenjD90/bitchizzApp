f76e9f4426269b71aa4c4b636529126fa2ff6562
Benjamin <BenjDaniel90@gmail.com>
2016-01-14 00:02:22 +0100

    [event] Add futures events to home page.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,15 @@
  * Controller of the bitchizzApp
  */
 angular.module('bitchizzApp')
-  .controller('MainCtrl', function ($scope, Auth) {
+  .controller('MainCtrl', function ($scope, Auth, Ref, $firebaseObject) {
     if (Auth.$getAuth()) {
       $scope.isConnected = true;
     } else {
       $scope.isConnected = false;
     }
+
+
+    $scope.eventsBitchizz = $firebaseObject(Ref.child('events'));
+
 
   });
---
+++ app/scripts/filters/futureevents.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name bitchizzApp.filter:futureEvents
+ * @function
+ * @description
+ * # futureEvents
+ * Filter in the bitchizzApp.
+ */
+angular.module('bitchizzApp')
+  .filter('futureEvents', function () {
+    return function (input) {
+      console.log(input);
+      var nowTimeStamp = new Date().getTime();
+      var ret = {};
+
+      for (var key in input) {
+        if (!key.startsWith('$')) {
+          if(input[key].date > nowTimeStamp) {
+            ret[key] = input[key];
+          }
+        }
+      }
+
+      return ret;
+    };
+  });
